614a61725f5e06fe828b8d2703ac837119c47b2b
Ruslana <garaeva-r_23@mail.ru>
2022-09-05 00:40:04 +0300

    Editing namespace

--- WarCraft/WarCraft/Archer.cs
+++ WarCraft/WarCraft/Archer.cs
@@ -1,5 +1,4 @@
-﻿using ConsoleWarcraftProj;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- WarCraft/WarCraft/Range.cs
+++ WarCraft/WarCraft/Range.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleWarcraftProj
+namespace WarCraft
 {
     class Range : Military
     {
