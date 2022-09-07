ae87eeb6e4ee53d7db324b5930bd4eab082e5390
FamtanK <98282716+FamtanK@users.noreply.github.com>
2022-09-07 19:58:58 +0300

    added a delegate to enable berserk mode

--- Warcraft/FootMan.cs
+++ Warcraft/FootMan.cs
@@ -2,23 +2,36 @@
 {
     public FootMan(int health, int cost, string name, int speed, int damage, 
         int attackSpeed, int armor) : base(health, cost, name, speed, damage,
-        attackSpeed, armor) { }
-
-    public override void GetDamage(int dmg)
+        attackSpeed, armor)
     {
-        base.GetDamage(dmg);
-        
-        if (health <= maxHealth / 3 && health > 0)
-        {
-            Berserker();
-        }
+        HealthChangedEvent += Berserker;
     }
+
+    private bool berserkMode;
 
     public void Berserker()
     {
-        attackSpeed += attackSpeed / 2;
-        damage += damage / 4;
-        Console.WriteLine($"{name} in berserker mode");
+        if (isDestroyed)
+        {
+            return;
+        }
+        
+        if (berserkMode && health > maxHealth / 3)
+        {
+            berserkMode = false;
+            attackSpeed -= attackSpeed / 3;
+            damage -= damage / 5;
+            Console.WriteLine($"{name} not in berserker mode");
+        }
+
+        else if (!berserkMode && health < maxHealth / 3)
+        {
+            attackSpeed += attackSpeed / 2;
+            damage += damage / 4;
+            berserkMode = true;
+            Console.WriteLine($"{name} in berserker mode");
+        }
+
     }
 
     public void Stun(Unit unit)
