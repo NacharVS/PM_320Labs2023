9042a20611bfef3eb6f78861b0a739a08c2c87f3
dobriy-yashcher <golovinsergey17@gmail.com>
2022-09-08 00:55:35 +0300

    test commit.

--- Labs320/Program.cs
+++ Labs320/Program.cs
@@ -1,7 +1,5 @@
 ﻿// Turushkin Sergey, 320P, "Warcraft"
 
-// test comm
-
 using Units;
 
 namespace Warcraft
@@ -10,8 +8,8 @@ namespace Warcraft
     {
         static void Main()
         {
-            var peasant = new Peasant(100, 1.5, "peasant", 2.4, 1);
-            var footman = new Footman(150, 2.2, "footman", 2.2, 1, 30, 1.1, 50);
+            var peasant = new Peasant(100, 1.5, "peasant", 1, 2.4);
+            var footman = new Footman(150, 2.2, "footman", 1, 2.2, 30, 1.1, 50);
             var mage = new Mage(50, 15, "mage", 1, 2, 50, 2, 10, 7, 80);
             var guardTower = new GuardTower(450, 50, "guardTower", 1, 5, 50, 1);
             var dragon = new Dragon(1000, 150, "dragon", 1, 5.5, 100, 3, 0, 8, 500);
@@ -32,7 +30,7 @@ namespace Warcraft
             }
 
 
-            {
+            /*{
                 var units = new List<Units.Unit> {peasant, footman, mage, guardTower, dragon};
 
                 while (units.Count > 1)
@@ -49,7 +47,7 @@ namespace Warcraft
                         //units[unit2].
                     }
                 }
-            }
+            }*/
         }
     }
 }
