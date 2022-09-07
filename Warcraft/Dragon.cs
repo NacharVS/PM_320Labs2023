363c18d538f982a1099766ec7d1e1e88c31220d0
FamtanK <98282716+FamtanK@users.noreply.github.com>
2022-09-07 19:57:34 +0300

    Passive skills converted into active skills

--- Warcraft/Dragon.cs
+++ Warcraft/Dragon.cs
@@ -4,19 +4,22 @@
 
     public void FireBreach(Unit unit)
     {
-        Random rnd = new Random();
-        int chance = rnd.Next(1, 6);    //chance to fire breach is 20%
-
-        if (chance == 1 && mana >= 10)  //fire breach need 10 mana to use
-        {
-            unit.GetFire(damage, 3);    //fire will attack unit by 3 moves
-            mana -= 10;
-        }
+        unit.GetFire(damage, 3);       //fire will attack unit by 3 moves
+        mana -= 10;
     }
 
     public override void Attack(Unit unit)
     {
-        FireBreach(unit);
-        base.Attack(unit);
+        Random rnd = new Random();
+        int chance = rnd.Next(1, 3);
+
+        if (chance == 1 && mana >= 10)  //fire breach need 10 mana to use
+        {
+            FireBreach(unit);
+        }
+        else
+        {
+            base.Attack(unit);
+        }
     }
 }
