222be87f928b2a7cf4ae0d1da6379a63a7e2f6d7
Rinat050 <safiullin.rinat.05@gmail.com>
2022-09-10 10:20:40 +0300

    Create Characters classes

--- CharacterEditor/CharacterEditor/CharacterEditorCore/Character.cs
+++ CharacterEditor/CharacterEditor/CharacterEditorCore/Character.cs
@@ -1,13 +1,13 @@
 ﻿namespace CharacterEditorCore
 {
-    abstract class Character
+    public abstract class Character
     {
         public Characterictic Strength { get; set; }
         private double _strengthAttackChange;
         private double _strengthHealthChange;
         public Characterictic Dexterity { get; set; }
         private double _dexterityAttackChange;
-        private double _dexterityPhysicalDefenceChange;
+        protected double _dexterityPhysicalDefenceChange;
         public Characterictic Constitution { get; set; }
         private double _constitutionHealthChange;
         private double _constitutionPhysicalDefenceChange;
@@ -15,7 +15,7 @@
         private double _intellisenseManaChange;
         private double _intellisenseMagicalAttackChange;
 
-        public int Mana
+        public double Mana
         {
             get { return Mana; }
             set
@@ -27,7 +27,7 @@
                 Mana = value;
             }
         }
-        public int AttackDamage
+        public double AttackDamage
         {
             get { return AttackDamage; }
             set
@@ -40,7 +40,7 @@
             }
         }
 
-        public int Health
+        public double Health
         {
             get { return Health; }
             set
@@ -53,7 +53,7 @@
             }
         }
 
-        public int PhysicalDefence
+        public double PhysicalDefence
         {
             get { return PhysicalDefence; }
             set
@@ -66,7 +66,7 @@
             }
         }
 
-        public int MagicalAttackDamage
+        public double MagicalAttackDamage
         {
             get { return MagicalAttackDamage; }
             set
@@ -77,6 +77,33 @@
                 }
                 MagicalAttackDamage = value;
             }
+        }
+
+        protected Character(Characterictic strength,
+                            double strengthAttackChange,
+                            double strengthHealthChange,
+                            Characterictic dexterity,
+                            double dexterityAttackChange,
+                            double dexterityPhysicalDefenceChange,
+                            Characterictic constitution,
+                            double constitutionHealthChange,
+                            double constitutionPhysicalDefenceChange,
+                            Characterictic intellisense,
+                            double intellisenseManaChange,
+                            double intellisenseMagicalAttackChange)
+        {
+            Strength = strength;
+            _strengthAttackChange = strengthAttackChange;
+            _strengthHealthChange = strengthHealthChange;
+            Dexterity = dexterity;
+            _dexterityAttackChange = dexterityAttackChange;
+            _dexterityPhysicalDefenceChange = dexterityPhysicalDefenceChange;
+            Constitution = constitution;
+            _constitutionHealthChange = constitutionHealthChange;
+            _constitutionPhysicalDefenceChange = constitutionPhysicalDefenceChange;
+            Intellisense = intellisense;
+            _intellisenseMagicalAttackChange = intellisenseMagicalAttackChange;
+            _intellisenseManaChange = intellisenseManaChange;
         }
     }
 }
---
+++ CharacterEditor/CharacterEditor/CharacterEditorCore/Warrior.cs
@@ -0,0 +1,10 @@
+﻿namespace CharacterEditorCore
+{
+    public class Warrior : Character
+    {
+        public Warrior() : base(new Characterictic(30,250), 5, 2,
+                                new Characterictic(15, 70), 1, 1,
+                                new Characterictic(20, 100), 10, 2,
+                                new Characterictic(10, 50), 1, 1) {}
+    }
+}
