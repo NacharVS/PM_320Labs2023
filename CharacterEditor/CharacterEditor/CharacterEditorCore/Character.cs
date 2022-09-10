f6008ea761ade1a306dd44867186ec680d72228c
Rinat050 <safiullin.rinat.05@gmail.com>
2022-09-10 09:12:47 +0300

    Create CharacterEditorCore and add Base classes

---
+++ CharacterEditor/CharacterEditor/CharacterEditorCore/Character.cs
@@ -0,0 +1,78 @@
+﻿namespace CharacterEditorCore
+{
+    abstract class Character
+    {
+        public Characterictic Strength { get; set; }
+        public Characterictic Dexterity { get; set; }
+        public Characterictic Constitution { get; set; }
+        public Characterictic Intellisense { get; set; }
+
+        public int AttackDamage
+        {
+            get { return AttackDamage; }
+            set
+            {
+                if (value <= 0)
+                {
+                    AttackDamage = 0;
+                }
+                AttackDamage = value;
+            }
+        }
+
+        public int Health
+        {
+            get { return Health; }
+            set
+            {
+                if (value <= 0)
+                {
+                    Health = 0;
+                }
+                Health = value;
+            }
+        }
+
+        public int PhysicalDefence
+        {
+            get { return PhysicalDefence; }
+            set
+            {
+                if (value <= 0)
+                {
+                    PhysicalDefence = 0;
+                }
+                PhysicalDefence = value;
+            }
+        }
+
+        public int MagicalAttackDamage
+        {
+            get { return MagicalAttackDamage; }
+            set
+            {
+                if (value <= 0)
+                {
+                    MagicalAttackDamage = 0;
+                }
+                MagicalAttackDamage = value;
+            }
+        }
+
+        private int _attackChange;
+        private int _healthChange;
+        private int _physicalDefenceChange;
+        private int _magicalAttackChange;
+
+        protected Character(Characterictic strength, 
+                            Characterictic dexterity, 
+                            Characterictic constitution, 
+                            Characterictic intellisense)
+        {
+            Strength = strength;
+            Dexterity = dexterity;
+            Constitution = constitution;
+            Intellisense = intellisense;
+        }
+    }
+}
---
+++ CharacterEditor/CharacterEditor/CharacterEditorCore/Characterictic.cs
@@ -0,0 +1,51 @@
+﻿namespace CharacterEditorCore
+{
+    public class Characterictic
+    {
+        private int _value;
+        private int _minValue;
+        private int _maxValue;
+
+        public Characterictic(int value, int minValue, int maxValue)
+        {
+            MaxValue = maxValue;
+            MinValue = minValue;
+            Value = value;
+        }
+
+        public int MaxValue
+        {
+            get { return _maxValue; }
+            set
+            {
+                if (value >= _minValue)
+                {
+                    _maxValue = value;
+                }
+            }
+        }
+
+        public int MinValue
+        {
+            get { return _minValue; }
+            set
+            {
+                if (value <= _maxValue)
+                {
+                    _minValue = value;
+                }
+            }
+        }
+        public int Value
+        {
+            get { return _value; }
+            set 
+            {
+                if (value >= _minValue && value <= _maxValue)
+                {
+                    _value = value;
+                }
+            }
+        }
+    }
+}
