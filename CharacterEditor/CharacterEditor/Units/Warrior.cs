5658090e5179ac9139a5f610e9b17b3369feb6a0
Amenisk <106079585+Amenisk@users.noreply.github.com>
2022-09-11 17:09:10 +0300

    Add files via upload

---
+++ CharacterEditor/CharacterEditor/Units/Warrior.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Characters
+{
+    public class Warrior : Character
+    {
+        public new int Strength 
+        { 
+            get { return strength; }
+            set 
+            {  
+                if (value > maxStrength)
+                {
+
+                }
+                else
+                {
+                    Damage += (value - strength) * 5;
+                    HP += (value - strength) * 2;
+
+                    strength = value;
+                }
+
+            }
+        }
+        public new int Dexterity 
+        {
+            get { return dexterity; }
+            set
+            {
+                if (value > maxDexterity)
+                {
+
+                }
+                else
+                {
+                    Damage += value - dexterity;
+                    PhysDef += value - dexterity;
+
+                    dexterity = value;
+                }
+
+            }
+        }
+        public new int Constitution 
+        {
+            get { return constitution; }
+            set
+            {
+                if (value > maxConstitution)
+                {
+
+                }
+                else
+                {
+                    HP += (value - constitution) * 10; 
+                    PhysDef += (value - constitution) * 2;
+
+                    constitution = value;
+                }
+
+            }
+        }
+        public new int Inteligence 
+        {
+            get { return inteligence; }
+            set
+            {
+                if (value > maxInteligence)
+                {
+
+                }
+                else
+                {
+                    MP += value - inteligence;
+                    MagDamage += value - inteligence;
+
+                    inteligence = value;
+                }
+
+            }
+        }
+
+        public Warrior()
+        {
+            maxStrength = 250;
+            maxDexterity = 70;
+            maxConstitution = 100;
+            maxInteligence = 50;
+            Strength = 30;
+            Dexterity = 15;
+            Constitution = 20;
+            Inteligence = 10;
+        }
+
+    }
+}
---
+++ CharacterEditor/CharacterEditor/Units/Wizard.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Characters
+{
+    public class Wizard:Character
+    {
+        public new int Strength
+        {
+            get { return strength; }
+            set
+            {
+                if (value > maxStrength)
+                {
+
+                }
+                else
+                {
+                    Damage += (value - strength) * 3;
+                    HP += value - strength;
+
+                    strength = value;
+                }
+
+            }
+        }
+        public new int Dexterity
+        {
+            get { return dexterity; }
+            set
+            {
+                if (value > maxDexterity)
+                {
+
+                }
+                else
+                {
+                    PhysDef += (value - dexterity) * 0.5;
+
+                    dexterity = value;
+                }
+
+            }
+        }
+        public new int Constitution
+        {
+            get { return constitution; }
+            set
+            {
+                if (value > maxConstitution)
+                {
+
+                }
+                else
+                {
+                    HP += (value - constitution) * 3;
+                    PhysDef += value - constitution;
+
+                    constitution = value;
+                }
+
+            }
+        }
+        public new int Inteligence
+        {
+            get { return inteligence; }
+            set
+            {
+                if (value > maxInteligence)
+                {
+
+                }
+                else
+                {
+                    MP += (value - inteligence) * 2;
+                    MagDamage += (value - inteligence) * 5;
+
+                    inteligence = value;
+                }
+
+            }
+        }
+        public Wizard()
+        {
+            maxStrength = 45;
+            maxDexterity = 70;
+            maxConstitution = 60;
+            maxInteligence = 250;
+            Strength = 10;
+            Dexterity = 20;
+            Constitution = 15;
+            Inteligence = 35;
+        }
+    }
+}
