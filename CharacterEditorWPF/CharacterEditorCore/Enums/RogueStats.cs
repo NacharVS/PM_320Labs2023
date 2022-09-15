1881f39daad28feaf1e5620d8e46ef56f39d99ec
riazonbin <84237219+riazonbin@users.noreply.github.com>
2022-09-15 12:09:30 +0300

    added second solution file

---
+++ CharacterEditorWPF/CharacterEditorCore/Enums/RogueStats.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterEditorCore.Enums
+{
+    public enum RogueStats
+    {
+        minStrength = 10,
+        maxStrength = 55,
+        minDexterity = 30,
+        maxDexterity = 250,
+        minConstitution = 20,
+        maxConstitution = 80,
+        minIntelligence = 15,
+        maxIntelligence = 75
+    }
+}
