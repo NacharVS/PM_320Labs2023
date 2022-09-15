fb4eeac7fb61adda37782d314166f3aa44ea20a9
riazonbin <riazonbin@mail.ru>
2022-09-15 08:31:51 +0300

    added .sln with character editor

---
+++ CharacterEditorCore/Enums/WarriorStats.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterEditorCore.Enums
+{
+    public enum WarriorStats
+    {
+        minStrength = 30,
+        maxStrength = 250,
+        minDexterity = 15,
+        maxDexterity = 70,
+        minConstitution = 20,
+        maxConstitution = 100,
+        minIntelligence = 10,
+        maxIntelligence = 50
+    }
+}
