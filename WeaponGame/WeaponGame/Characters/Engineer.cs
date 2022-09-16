e7a1956ebef29de8782d9aa64c95c88601f18381
Rinat050 <safiullin.rinat.05@gmail.com>
2022-09-16 21:15:29 +0300

    Add Characters

---
+++ WeaponGame/WeaponGame/Characters/Engineer.cs
@@ -0,0 +1,17 @@
+﻿namespace WeaponGame.Characters
+{
+    public class Engineer
+    {
+        public string Name { get; }
+
+        public Engineer(string name)
+        {
+            Name = name;
+        }
+
+        public void Repair(IRepairible item)
+        {
+            item.Repair();
+        }
+    }
+}
---
+++ WeaponGame/WeaponGame/Characters/Warrior.cs
@@ -0,0 +1,43 @@
+﻿namespace WeaponGame.Characters
+{
+    public class Warrior
+    {
+        private List<IWeapon> inventory = new List<IWeapon>();
+        public string Name { get; }
+
+        public Warrior(string name)
+        {
+            Name = name;
+        }
+
+        public void PickUpWeapon(IWeapon weapon)
+        {
+            inventory.Add(weapon);
+        }
+
+        public void Attack(IWeapon weapon)
+        {
+            weapon.SingleShoot();
+        }
+
+        public void MultiAttack(ITripleShoot weapon)
+        {
+            weapon.TripleShoot();
+        }
+
+        public void MeleeAttack(IMeleeWeapon weapon)
+        {
+            weapon.MeleeAttack();
+        }
+
+        public void Throw(IThrowable weapon)
+        {
+            weapon.Throw();
+        }
+
+        public void ReloadItem(IReloadable item)
+        {
+            item.Reload();
+        }
+    }
+}
