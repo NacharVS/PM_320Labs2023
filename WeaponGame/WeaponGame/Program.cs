ab30e825ffde32f8b692602a83574c95ec295582
bezoksy <96592214+bezoksy@users.noreply.github.com>
2022-09-19 11:16:54 +0300

    shell of the WeaponGame(with interfaces)

---
+++ WeaponGame/WeaponGame/Program.cs
@@ -0,0 +1,15 @@
+﻿using WeaponGame_Core.Characters;
+using WeaponGame_Core.Weapons;
+
+namespace WeaponGameConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Warrior warrior1 = new Warrior();
+            Warrior warrior2 = new Warrior();
+            Engineer enginer = new Engineer();
+        }
+    }
+}
