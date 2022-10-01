58cb302dcb59b5463ed82e1cd09e0b3affa072a6
Abramov Danil <112745010+steelbsteel@users.noreply.github.com>
2022-10-01 10:14:52 +0300

    Interface practice
    done

---
+++ Shooter/Weapons/Kunai.cs
@@ -0,0 +1,29 @@
+﻿using InterfacesWar_Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesWar_Core.Weapons
+{
+    internal class Kunai : IThrowable
+    {
+        int damage = 17;
+        string name = "unsigned";
+        public Kunai(string newName)
+        {
+            name = newName;
+        }
+        public void Throw()
+        {
+            Console.WriteLine($"Kunai has been throwed! Damage given: {damage}");
+        }
+
+        public void Upgrade()
+        {
+            damage += 2;
+            Console.WriteLine("Kunai was upgraded by 2 DP");
+        }
+    }
+}
---
+++ Shooter/Weapons/Sword.cs
@@ -0,0 +1,31 @@
+﻿using InterfacesWar_Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesWar_Core.Weapons
+{
+    internal class Sword : IMelee
+    {
+        int damage = 20;
+        string name = "unsigned";
+
+        public Sword(string newName)
+        {
+            name = newName;
+        }
+
+        public void MeleeAttack()
+        {
+            Console.WriteLine($"Sword attack! Damage given: {damage}");
+        }
+
+        public void Upgrade()
+        {
+            damage += 20;
+            Console.WriteLine($"Sword was upgraded by 20 DP");
+        }
+    }
+}
