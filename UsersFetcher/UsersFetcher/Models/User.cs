0041b95f3ebe860be7f226be605b318549383369
b.kotdusov <kotdusovb@gmail.com>
2022-10-19 20:01:35 +0300

    Simple users service

---
+++ UsersFetcher/UsersFetcher/Models/User.cs
@@ -0,0 +1,7 @@
+﻿namespace UsersFetcher.Models;
+
+public class User
+{
+    public string? Login { get; set; }
+    public int Age { get; set; }
+}
---
+++ UsersFetcher/UsersFetcher/Services/UserService.cs
@@ -0,0 +1,27 @@
+﻿using UsersFetcher.Models;
+
+namespace UsersFetcher.Services;
+
+public class UserService
+{
+    public Task<User[]> GetUsers()
+    {
+        return Task.FromResult(Enumerable.Range(1, 1).Select(_ => new User
+        {
+            Age = Random.Shared.Next(14, 60),
+            Login = GenerateLogin()
+        }).ToArray());
+    }
+
+    private string GenerateLogin()
+    {
+        var chars = new List<char>();
+
+        for (var i = 0; i < Random.Shared.Next(3, 12); ++i)
+        {
+            chars.Add((char)Random.Shared.Next(65, 120));
+        }
+
+        return String.Concat(chars);
+    }
+}
