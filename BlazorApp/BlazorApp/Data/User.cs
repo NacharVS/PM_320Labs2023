9fd80369fb67e8a3e2bf471fbcd10ed367620a77
Ruslana <garaeva-r_23@mail.ru>
2022-10-19 23:51:43 +0300

    Adding Users

---
+++ BlazorApp/BlazorApp/Data/User.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorApp.Data;
+
+public class User
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int Age { get; set; }
+}
---
+++ BlazorApp/BlazorApp/Data/UserService.cs
@@ -0,0 +1,26 @@
+﻿namespace BlazorApp.Data;
+
+public class UserService
+{
+    private static readonly string[] Names = new[]
+    {
+        "Billy", "Mary", "Phyllip", "Joseph", "Ernest",
+        "Josephine", "Dan", "Pauline", "Elizabeth", "Norma"
+    };
+
+    private static readonly string[] Surnames = new[]
+    {
+        "Henderson", "Thompson", "Murray", "Smith", "Johnson",
+        "Davis", "Ross", "Hunter", "Lambert", "Scott"
+    };
+    
+    public Task<Data.User[]> GetUserAsync(DateTime startDate)
+    {
+        return Task.FromResult(Enumerable.Range(1, 5).Select(index => new Data.User
+        {
+            Name = Names[Random.Shared.Next(Names.Length)],
+            Surname = Surnames[Random.Shared.Next(Surnames.Length)],
+            Age = Random.Shared.Next(19,51)
+        }).ToArray());
+    }
+}
