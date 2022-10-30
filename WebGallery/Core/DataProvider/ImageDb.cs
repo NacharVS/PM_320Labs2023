4d4dd0fb66b5541d721c987e3ddb426f0ae57bf0
b.kotdusov <kotdusovb@gmail.com>
2022-10-31 02:49:31 +0300

    Gallery app

---
+++ WebGallery/Core/DataProvider/ImageDb.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Win32;
+using MongoDB.Driver;
+using MongoDB.Driver.GridFS;
+
+namespace Core.DataProvider;
+
+public static class ImageDb
+{
+    private static readonly MongoClient Client = new("mongodb://localhost");
+    private static readonly IMongoDatabase Database = Client.GetDatabase("Image");
+    private static readonly GridFSBucket FileWorker = new(Database);
+    private static readonly string DownloadsFolder = GetDownloadFolderPath();
+
+    public static async Task UploadImageToDb(string fileName)
+    {
+        var fileInfo =
+            await FileWorker.FindAsync(Builders<GridFSFileInfo>.Filter.Eq(info => info.Filename, fileName));
+
+        if (fileInfo.FirstOrDefault() is null)
+        {
+            await using FileStream fs = new FileStream($"{Directory.GetCurrentDirectory()}/wwwroot/upload/{fileName}", FileMode.Open);
+            await FileWorker.UploadFromStreamAsync(fileName, fs);
+        }
+    }
+
+    public static async Task DownloadToLocal(string fileName)
+    {
+        try
+        {
+            await using FileStream fs = new FileStream(
+                $"{DownloadsFolder}/{fileName}",
+                FileMode.Create);
+            await FileWorker.DownloadToStreamByNameAsync(fileName, fs);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    
+    private static string GetDownloadFolderPath() 
+    {
+        return Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders", 
+            "{374DE290-123F-4565-9164-39C4925E467B}", String.Empty)?.ToString() ?? "downloads";
+    }
+}
---
+++ WebGallery/Web/Services/FileService.cs
@@ -0,0 +1,27 @@
+﻿using Core.DataProvider;
+
+namespace Web.Services;
+
+public class FileService
+{
+
+
+    public async Task DownloadImage(string fileName)
+    {
+        await ImageDb.DownloadToLocal(fileName);
+    }
+
+    public List<string> LoadAllImages()
+    {
+        return Directory.EnumerateFiles("wwwroot/upload")
+            .Where(x => x.Contains(".jpg")).Select(x => x.Split("\\")[^1])
+            .ToList();
+    }
+    public async Task UploadAllImages()
+    {
+        foreach (var filePath in LoadAllImages())
+        {
+            await ImageDb.UploadImageToDb(filePath);
+        }
+    }
+}
