4100bed70b6304930398f3ffa3cae0c54945a849
Flictivity <bulat_sa1@mail.ru>
2022-11-10 22:19:41 +0300

    Add Reg page

---
+++ DocumentApp/DocumentApp/DataBaseConnection.cs
@@ -0,0 +1,14 @@
+﻿using DocumentApp.Models;
+using MongoDB.Driver;
+
+namespace DocumentApp;
+
+public static class DataBaseConnection
+{
+    private static readonly MongoClient Client = new MongoClient("mongodb://localhost");
+    private static readonly IMongoDatabase Db = Client.GetDatabase("DocumentsDb");
+    public static readonly IMongoCollection<User> UsersCollection = Db.GetCollection<User>("Users");
+    //public static readonly IMongoCollection<User> DocumentsCollection = Db.GetCollection<User>("Documents");
+    public static readonly IMongoCollection<Role> RolesCollection = Db.GetCollection<Role>("Roles");
+    //public static readonly IMongoCollection<User> ProjectsCollection = Db.GetCollection<User>("Project");
+}
