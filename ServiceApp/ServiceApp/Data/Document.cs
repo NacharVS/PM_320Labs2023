0a3031a5396aa9766cba5ca89369dd97407dac8c
Ruslana <garaeva-r_23@mail.ru>
2022-11-17 09:53:21 +0300

    Edit

--- ServiceApp/ServiceApp/Data/Document.cs
+++ ServiceApp/ServiceApp/Data/Document.cs
@@ -3,7 +3,7 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace ServiceApp.Data;
 
-public class Document
+public class Document : Project
 {
     [BsonId]
     [BsonIgnoreIfDefault]
@@ -13,17 +13,14 @@ public class Document
     public string? Name { get; set; }
     [BsonIgnoreIfDefault]
     [BsonIgnoreIfNull]
-    public string? Number { get; set; }
-    [BsonIgnoreIfDefault]
-    [BsonIgnoreIfNull]
-    public string? designerOrganization;
-    [BsonIgnoreIfDefault]
-    [BsonIgnoreIfNull]
-    public string? developerOrganization;
-    [BsonIgnoreIfDefault]
-    [BsonIgnoreIfNull]
     public bool? IsApproved { get; set; }
     [BsonIgnoreIfDefault]
     [BsonIgnoreIfNull]
     public bool? IsRequired { get; set; }
+
+    /*public Document(bool isApproved, bool isRequired)
+    {
+        IsApproved = isApproved;
+        IsRequired = isRequired;
+    }*/
 }
